896bccd7cb7df1e0a1a31eb42337bbf3818e0d6a
Mountain Cat <tomasz.banas123@gmail.com>
2023-04-23 10:42:12 +0200

    added get claims endopint

---
+++ Backend/ManageMe/ManageMe.Api/Controllers/Claims.cs
@@ -0,0 +1,27 @@
+﻿using ManageMe.Application.Features.GetClaims;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageMe.Api.Controllers;
+
+public class ClaimsController : Controller
+{
+    private IMediator _mediator;
+
+    public ClaimsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> GetClaims()
+    {
+        var mediatorResuest = new GetClaimsRequest(User);
+
+        var resultDto = await _mediator.Send(mediatorResuest);
+
+        return Ok(resultDto);
+    }
+}
