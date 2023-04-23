55dbf2ba0a2fe14d1b0c900c09039fc013630047
Mountain Cat <tomasz.banas123@gmail.com>
2023-04-23 10:38:27 +0200

    Added google authentication endpoints

---
+++ Backend/ManageMe/ManageMe.Api/Controllers/GoogleAuthentication.cs
@@ -0,0 +1,42 @@
+﻿using ManageMe.Api.Extensions;
+using ManageMe.Application.Features.GoogleAuthentication;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageMe.Api.Controllers;
+
+[ApiController]
+[Route("api/authentication/google")]
+public class GoogleAuthentication : Controller
+{
+    private IMediator _mediator;
+
+    public GoogleAuthentication(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] AuthiViaGoogleRequestContract contract)
+    {
+        var mediatorRequest = new CreateGoogleAccountRequest(contract.AuthToken);
+
+        var result = await _mediator.Send(mediatorRequest);
+
+        return result.ToOk();
+    }
+
+
+    [HttpPost("auth")]
+    public async Task<IActionResult> Authenticate([FromBody] AuthiViaGoogleRequestContract contract)
+    {
+        var mediatorRequest = new AuthiViaGoogleRequest
+        {
+            GoogleAuthToken = contract.AuthToken
+        };
+
+        var result = await _mediator.Send(mediatorRequest);
+
+        return result.ToOk();
+    }
+}
---
+++ Backend/ManageMe/ManageMe.Api/Extensions/ResultExtensions.cs
@@ -0,0 +1,69 @@
+﻿using Catut;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageMe.Api.Extensions;
+
+public static class ResultExtensions
+{
+    public static IActionResult ToOk<TResult, TContract>(
+        this Result<TResult> result, Func<TResult, TContract> mapper, int statusCode = 200)
+    {
+        return result.Match<IActionResult>(
+            Succ: obj =>
+            {
+                var response = mapper(obj);
+                return new ObjectResult(response)
+                {
+                    StatusCode = statusCode
+                };
+            },
+            Fail: exception =>
+            {
+                return ProcessFail(exception);
+            });
+    }
+    
+    public static IActionResult ToOk<TResult>(
+        this Result<TResult> result, int statusCode = 200)
+    {
+        return result.Match<IActionResult>(
+            Succ: obj =>
+            {
+                return new StatusCodeResult(statusCode);
+            },
+            Fail: exception =>
+            {
+                return ProcessFail(exception);
+            });
+    }
+    
+    public static IActionResult ToOk(
+        this Result result, int statusCode = 200)
+    {
+        return result.Match<IActionResult>(
+            Succ: () =>
+            {
+                return new StatusCodeResult(statusCode);
+            },
+            Fail: exception =>
+            {
+                return ProcessFail(exception);
+            });
+    }
+
+    private static IActionResult ProcessFail(Exception exception)
+    {
+        if (exception is ValidationException validationException)
+        {
+            return new BadRequestObjectResult(validationException);
+        }
+                
+        if (exception is UnauthorizedAccessException ex)
+        {
+            return new UnauthorizedObjectResult(ex);
+        }
+        
+        return new StatusCodeResult(500);
+    }
+}
